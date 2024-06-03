4bcd7ab59b78ca0e1b0eae3fe5dfa27837b30662
z3ttee <cedric.zitzmann@gmail.com>
2024-06-03 20:12:57 +0200

    moved to use navbutton

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import pageInsight from "astro-page-insight";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon(), pageInsight()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				body: 'hsl(50, 72%, 97%)',
-				'body-on': 'hsl(330, 7%, 5%)'
-			},
-			spacing: {
-				'window-sm': '24px',
-				'window': '48px'
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        body: "hsl(50, 72%, 97%)",
+        "body-on": "hsl(330, 7%, 5%)",
+        outline: "hsl(330, 7%, 40%)"
+      },
+      spacing: {
+        "window-sm": "24px",
+        window: "48px"
+      }
+    }
+  },
+  plugins: []
+};
