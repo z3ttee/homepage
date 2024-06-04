2ad818a33e3e37da0f514b2ba277ed7e61a5529c
z3ttee <cedric.zitzmann@gmail.com>
2024-06-04 16:12:12 +0200

    validate available languages

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,14 @@
 import { atom } from "nanostores";
-import { defaultLang } from "./i18n/ui";
+import { defaultLang, messages } from "./i18n/ui";
 
 export const $isNavigationMenuOpen = atom(false);
 export const $locale = atom(defaultLang);
 
 export function setLocale(locale: string | null) {
-  $locale.set(locale ?? defaultLang);
+  locale = locale?.toLowerCase() ?? defaultLang;
+  if (!messages[locale]) {
+    locale = defaultLang;
+  }
+
+  $locale.set(locale);
 }
