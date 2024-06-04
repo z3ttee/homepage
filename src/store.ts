4a7db06bc388ba607432bc879e813a0c2847b2b9
z3ttee <cedric.zitzmann@gmail.com>
2024-06-04 14:25:33 +0200

    added locale selection

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,8 @@
 import { atom } from "nanostores";
 
 export const $isNavigationMenuOpen = atom(false);
+export const $locale = atom("EN");
+
+export function setLocale(locale: string | null) {
+  $locale.set(locale ?? "EN");
+}
